3b2c3ffc23a399d0bab28fc186b0b322ed0705d3
nnikitochka <asopiasoska@gmail.com>
2025-06-06 17:21:03 +0700

    Добавлены комментарии

--- src/main/kotlin/nn/edition/utils/timeformat/TimeFormat.kt
+++ src/main/kotlin/nn/edition/utils/timeformat/TimeFormat.kt
@@ -1,10 +1,25 @@
 package nn.edition.utils.timeformat
 
 data class TimeFormat(
+    /**
+     * Первая форма времени.
+     * Пример: 1 год, 1 месяц.
+     */
     val firstFormat: String,
+    /**
+     * Вторая форма времени.
+     * Пример: 2 года, 2 месяца.
+     */
     val secondFormat: String,
+    /**
+     * Третья форма времени.
+     * Пример: 10 лет, 10 месяцев.
+     */
     val thirdFormat: String,
 ) {
+    /**
+     * Форматы времени по умолчанию.
+     */
     companion object {
         val yearFormat = TimeFormat("год", "года", "лет")
         val monthFormat = TimeFormat("месяц", "месяца", "месяцев")
